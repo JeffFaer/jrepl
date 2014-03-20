24aa44f3ad1c0e77b69437e100b12ab7a7d73989
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-03-20 18:47:37 -0400

    Removed an unnecessary module

--- src/main/java/falgout/jrepl/command/CommandFactory.java
+++ src/main/java/falgout/jrepl/command/CommandFactory.java
@@ -1,7 +1,10 @@
 package falgout.jrepl.command;
 
+import com.google.inject.ImplementedBy;
+
 import falgout.jrepl.Environment;
 
+@ImplementedBy(JavaCommandFactory.class)
 public interface CommandFactory<R> {
     public Command<? extends R> getCommand(Environment env, String input);
 }
--- src/test/java/falgout/jrepl/guice/TestModule.java
+++ src/test/java/falgout/jrepl/guice/TestModule.java
@@ -7,24 +7,22 @@ import com.google.common.io.CharSource;
 import com.google.inject.AbstractModule;
 
 import falgout.jrepl.EnvironmentModule;
-import falgout.jrepl.command.JavaCommandModule;
 
 public class TestModule extends AbstractModule {
     public TestModule() {}
-    
+
     @Override
     protected void configure() {
         CharArrayWriter out = new CharArrayWriter();
         CharArrayWriter err = new CharArrayWriter();
-
+        
         bind(CharArrayWriter.class).annotatedWith(Stdout.class).toInstance(out);
         bind(CharArrayWriter.class).annotatedWith(Stderr.class).toInstance(err);
-        
+
         try {
             install(new EnvironmentModule(CharSource.empty().openStream(), out, err));
         } catch (IOException e) {
             throw new Error(e);
         }
-        install(new JavaCommandModule());
     }
 }
