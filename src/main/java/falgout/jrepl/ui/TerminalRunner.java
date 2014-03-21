12a5d66ac84b49a65e9b051bd9b841650f171c5b
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-03-21 13:35:03 -0400

    Refactored the TerminalRunner loop

--- src/main/java/falgout/jrepl/ui/TerminalRunner.java
+++ src/main/java/falgout/jrepl/ui/TerminalRunner.java
@@ -13,18 +13,14 @@ public class TerminalRunner {
     public static void main(String[] args) throws IOException {
         Injector injector = Guice.createInjector(new EnvironmentModule());
         Environment e = injector.getInstance(Environment.class);
-        
+
+        String prompt = "java: ";
         BufferedReader in = e.getInput();
         StringBuilder input = new StringBuilder();
         int braces = 0;
         String line;
-        do {
-            System.out.print("> ");
-            line = in.readLine();
-            if (line == null) {
-                break;
-            }
-
+        System.out.print(prompt);
+        while ((line = in.readLine()) != null && !line.equals("=exit")) {
             for (int i = 0; i < line.length(); i++) {
                 char ch = line.charAt(i);
                 if (ch == '{') {
@@ -33,17 +29,18 @@ public class TerminalRunner {
                     braces--;
                 }
             }
-            
+
             if (input.length() != 0) {
                 input.append("\n");
             }
             input.append(line);
-            
+
             if (braces == 0) {
                 e.execute(input.toString());
                 input = new StringBuilder();
             }
-        } while (!line.equals("=exit"));
 
+            System.out.print(prompt);
+        }
     }
 }
