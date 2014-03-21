807d8c7ad41b37b3cd046ca9e2031d50eb7fe042
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-03-21 19:20:47 -0400

    Refactored EnvironmentTests

--- src/test/java/falgout/jrepl/EnvironmentTest.java
+++ src/test/java/falgout/jrepl/EnvironmentTest.java
@@ -2,7 +2,6 @@ package falgout.jrepl;
 
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.hasItem;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertThat;
@@ -17,7 +16,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import com.google.common.reflect.TypeToken;
 import com.google.inject.Inject;
 
 import falgout.jrepl.guice.TestEnvironment;
@@ -29,37 +27,28 @@ import falgout.jrepl.reflection.Types;
 public class EnvironmentTest {
     @Inject @Rule public TestEnvironment env;
     @Inject public Environment e;
-
-    @Test
-    public void localVariablesAreAccessible() throws IOException {
-        env.executeNoErrors("int x = 5;");
-
-        TypeToken<?> type = TypeToken.of(int.class);
-        assertTrue(e.containsVariable("x"));
-        assertEquals(5, e.getVariable("x", type));
-    }
-
+    
     @Test
     public void javaLangIsImportedByDefault() throws IOException {
         Set<Import> imports = e.getImports();
-        assertThat(imports, contains(ImportTest.create(env, "import java.lang.*;").toArray(new Import[1])));
-    }
-
-    @Test
-    public void canAddImports() throws IOException {
-        env.executeNoErrors("import java.util.List;");
-        
-        assertThat(e.getImports(), hasItem(ImportTest.create(env, "import java.util.List;").get(0)));
+        assertThat(imports, contains(Import.create(false, "java.lang", true)));
     }
     
+    @Test
+    public void canAddImports() throws IOException {
+        Import i = Import.create(false, "java.util", true);
+        e.getImports().add(i);
+        assertThat(e.getImports(), hasItem(i));
+    }
+
     @Test
     public void cannotHaveDuplicateVariables() {
         Variable<?> var1 = new Variable<>(Types.OBJECT, "foo", new Object());
         Variable<?> var2 = new Variable<>(Types.OBJECT, "foo", new Object());
-        
+
         assertTrue(e.addVariable(var1));
         assertFalse(e.addVariable(var2));
-        
-        assertSame(var1.get(), e.getVariable("foo", Types.OBJECT));
+
+        assertSame(var1.get(), e.getVariable("foo").get());
     }
 }
