e44520c074907c54c0470766f05227afb3d26120
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-03-25 19:51:12 -0400

    Javadoc comment got dropped by accident

--- src/main/java/falgout/jrepl/command/execute/Executor.java
+++ src/main/java/falgout/jrepl/command/execute/Executor.java
@@ -7,6 +7,16 @@ import falgout.jrepl.Environment;
 
 @FunctionalInterface
 public interface Executor<I, R> {
+    /**
+     * Performs a value-bearing action on an input. This method modifies the
+     * {@code Environment} as needed. If, for any reason, the {@code input}
+     * cannot be executed, this method will throw an {@code ExecutionException}.
+     *
+     * @param env The {@code Environment} to execute in.
+     * @param input The {@code input} to execute
+     * @return A non-{@code null} result.
+     * @throws ExecutionException If an exception occurs during execution.
+     */
     public R execute(Environment env, I input) throws ExecutionException;
     
     default public <RR> Executor<I, RR> andThen(Function<? super R, ? extends RR> convert) {
