e1376c212daee79f7508a4f214edfb3ee43c92d9
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-03-26 15:17:47 -0400

    Added hashCode, equals, and toString to NestedClass

--- src/main/java/falgout/jrepl/reflection/NestedClass.java
+++ src/main/java/falgout/jrepl/reflection/NestedClass.java
@@ -38,4 +38,39 @@ public class NestedClass<T> implements Member {
     public boolean isSynthetic() {
         return clazz.isSynthetic();
     }
+    
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((clazz == null) ? 0 : clazz.hashCode());
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (!(obj instanceof NestedClass)) {
+            return false;
+        }
+        NestedClass<?> other = (NestedClass<?>) obj;
+        if (clazz == null) {
+            if (other.clazz != null) {
+                return false;
+            }
+        } else if (!clazz.equals(other.clazz)) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return clazz.toGenericString();
+    }
 }
