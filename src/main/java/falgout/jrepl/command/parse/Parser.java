44ed84d18cd75f2f54b1bde428ed14288c56fc89
Jeffrey Falgout <jeffrey.falgout@gmail.com>
2014-03-31 22:07:49 -0400

    Why not

--- src/main/java/falgout/jrepl/command/parse/Parser.java
+++ src/main/java/falgout/jrepl/command/parse/Parser.java
@@ -1,5 +1,13 @@
 package falgout.jrepl.command.parse;
 
-public interface Parser<I, R> {
+import java.util.function.Function;
+
+@FunctionalInterface
+public interface Parser<I, R> extends Function<I, R> {
     public R parse(I input);
+    
+    @Override
+    default public R apply(I t) {
+        return parse(t);
+    }
 }
